b616e094e9dc2a6baa5185956ce8b6aa5fc72e5f
Ravinderpal Singh <ravzdhanju@gmail.com>
2024-08-23 12:45:38 -0700

    Adding the add course feature

--- frontend/src/app/course-form/course-form.component.ts
+++ frontend/src/app/course-form/course-form.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Course, CourseService } from '../course.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { CURRENCIES } from '../constants/currencySymbols';
 
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
   templateUrl: './course-form.component.html',
-  styleUrl: './course-form.component.scss',
+  styleUrls: ['./course-form.component.scss'],
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
   course: Course = {
     _id: '',
     university: '',
@@ -24,7 +40,19 @@ export class CourseFormComponent {
     currency: '',
   };
 
-  constructor(private courseService: CourseService) {}
+  currencies: { code: string; name: string }[] = Object.entries(CURRENCIES).map(
+    ([code, details]) => ({
+      code,
+      name: details.name,
+    })
+  );
+
+  minDate: Date;
+
+  constructor(private courseService: CourseService) {
+    this.minDate = new Date(); // Set minDate to today
+  }
+  ngOnInit() {}
 
   onSubmit(): void {
     this.courseService.createCourse(this.course).subscribe({
