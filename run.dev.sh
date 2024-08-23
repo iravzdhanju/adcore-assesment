20cc0a1bc5bd267bbc94251f43e5c88f28086c28
Ravinderpal Singh <ravzdhanju@gmail.com>
2024-08-23 10:15:41 -0700

    Adding the dockerFile configs

--- run.dev.sh
+++ run.dev.sh
@@ -4,10 +4,6 @@
 echo "Stopping all running containers..."
 docker-compose down
 
-# Remove old images (optional, uncomment if needed)
-# echo "Removing old images..."
-# docker-compose rm -f
-
 # Build the images
 echo "Building Docker images..."
 docker-compose build
@@ -15,9 +11,3 @@ docker-compose build
 # Start the containers
 echo "Starting containers..."
 docker-compose up
-
-# To run in detached mode, use the following line instead:
-# docker-compose up -d
-
-# If running in detached mode, you might want to follow the logs:
-# docker-compose logs -f
